e19e66e81de74ae0368d685e6d9ab33c85da813f
Valdir Cezar <valdircezar312@gmail.com>
2021-04-29 14:12:53 -0300

    chore: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Header = () => {
-  return(
+  return (
     <div className="header">
-      <h2>To-do list with Typescript</h2>
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-dark p-4">
+          <h5 className="text-white h4">Todo list</h5>
+          <span className="text-muted">A To-do list app created with Typescript</span>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
