f00303a3cf21936096523967670d3413d93687cc
Valdir Cezar <valdircezar312@gmail.com>
2021-04-29 14:23:49 -0300

    feat: create TodoList component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Footer from './Footer';
 import Header from './Header'
+import TodoList from './TodoList/TodoList';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <TodoList />
       <Footer />
     </div>
   );
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+  return(
+    <div className="todo-list">
+      Todo list works!
+    </div>
+  )
+}
+
+export default TodoList;
