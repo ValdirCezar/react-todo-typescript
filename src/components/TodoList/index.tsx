f2b6aaa3435e5590020596e7a913a4f04d59287a
Valdir Cezar <valdircezar312@gmail.com>
2021-05-03 11:38:53 -0300

    feat: add img

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -8,13 +8,25 @@ const TodoList = () => {
   const list: Todo[] = [
     {
       id: 1,
-      title: 'Create Todo',
+      title: 'Create Todo 1',
       priority: 'alta',
-      done: false
+      done: true
     },
     {
       id: 2,
       title: 'Create Todo 2',
+      priority: 'alta',
+      done: false
+    },
+    {
+      id: 3,
+      title: 'Create Todo 3',
+      priority: 'alta',
+      done: true
+    },
+    {
+      id: 4,
+      title: 'Create Todo 4',
       priority: 'alta',
       done: false
     }
